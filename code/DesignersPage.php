395234e290274206fd88af702333508ff154c8e3
www-data <sunnysideupdevs@gmail.com>
2016-09-19 18:45:54 +1200

    fix ups

--- code/DesignersPage.php
+++ code/DesignersPage.php
@@ -1,24 +1,22 @@
 <?php
 
-class DesignersPage extends Page {
+class DesignersPage extends Page
+{
+    private static $icon = "designers/images/treeicons/DesignersPage";
 
-	private static $icon = "designers/images/treeicons/DesignersPage";
-
-	private static $many_many = array(
-		"Designers" => "Designer"
-	);
-
-
+    private static $many_many = array(
+        "Designers" => "Designer"
+    );
 }
 
-class DesignersPage_Controller extends Page_Controller {
-
-	function init() {
-		parent::init();
-		PrettyPhoto::include_code();
-		Requirements::themedCSS("DesignersPage", "designers");
-		Config::inst()->Update("PrettyPhoto", "theme", "dark_rounded");//"dark_rounded", "dark_square", "facebook", "light_rounded", "light_square" OR create your own!
-		Config::inst()->Update("PrettyPhoto", "more_config", "AnimationSpeed: 'slow', opacity: 0.30, showTitle: false");//see readme for some examples!
-	}
-
+class DesignersPage_Controller extends Page_Controller
+{
+    public function init()
+    {
+        parent::init();
+        PrettyPhoto::include_code();
+        Requirements::themedCSS("DesignersPage", "designers");
+        Config::inst()->Update("PrettyPhoto", "theme", "dark_rounded");//"dark_rounded", "dark_square", "facebook", "light_rounded", "light_square" OR create your own!
+        Config::inst()->Update("PrettyPhoto", "more_config", "AnimationSpeed: 'slow', opacity: 0.30, showTitle: false");//see readme for some examples!
+    }
 }
--- code/model/Designer.php
+++ code/model/Designer.php
@@ -1,36 +1,34 @@
 <?php
 
-class Designer extends DataObject {
+class Designer extends DataObject
+{
+    private static $db = array(
+        "Name" => "Varchar",
+        "Location" => "Varchar",
+        "Notes" => "Varchar(255)",
+        "Rate" => "Enum('$,$$,$$$,$$$$,$$$$$', '$$$')"
+    );
 
-	private static $db = array(
-		"Name" => "Varchar",
-		"Location" => "Varchar",
-		"Notes" => "Varchar(255)",
-		"Rate" => "Enum('$,$$,$$$,$$$$,$$$$$', '$$$')"
-	);
+    private static $has_one = array(
+        "Sample1" => "Image",
+        "Sample2" => "Image",
+        "Sample3" => "Image"
+    );
 
-	private static $has_one = array(
-		"Sample1" => "Image",
-		"Sample2" => "Image",
-		"Sample3" => "Image"
-	);
+    private static $belongs_many_many = array(
+        "DesignersPages" => "DesignersPage"
+    );
 
-	private static $belongs_many_many = array(
-		"DesignersPages" => "DesignersPage"
-	);
-
-	private static $default_order = "\"Rate\" ASC, \"Name\" ASC";
-
+    private static $default_order = "\"Rate\" ASC, \"Name\" ASC";
 }
 
-class Designer_Admin extends ModelAdmin {
+class Designer_Admin extends ModelAdmin
+{
+    private static $managed_models = array("Designer");
 
-	private static $managed_models = array("Designer");
+    private static $url_segment = 'designer';
 
-	private static $url_segment = 'designer';
+    private static $menu_title = 'Designers';
 
-	private static $menu_title = 'Designers';
-
-	public $showImportForm = false;
-
+    public $showImportForm = false;
 }
